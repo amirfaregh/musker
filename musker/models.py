44c7408d987432dc5be6a586a321925997302825
amirfaregh <amir.faregh@yahoo.com>
2023-04-26 19:18:34 +0300

    Added_profile_pic_to_homepage

--- musker/models.py
+++ musker/models.py
@@ -11,7 +11,13 @@ class Meep(models.Model):
     )
     body = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="meep_like", blank=True)
 
+    # Keep track or count of likes
+    def number_of_likes(self):
+        return self.likes.count()
+    
+    
     def __str__(self):
         return(
             f"{self.user} "
